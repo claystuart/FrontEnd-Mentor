71f94eecec29ba523425e916702701d6e3c6c7cb
Clay Stuart <clay.stuart@gmail.com>
2022-11-30 11:40:41 -0500

    Cleaned up header html and added styling to #menuSwitch

--- photosnap-multi-page/starter-code/script.js
+++ photosnap-multi-page/starter-code/script.js
@@ -4,13 +4,13 @@ document.querySelector('#menuSwitch').addEventListener('click', (e) => {
     if (menuClosed == true) {
         //Switch to menu open
         menuClosed = false;
-        e.currentTarget.src = "./assets/shared/mobile/close.svg";
+        e.target.src = "./assets/shared/mobile/close.svg";
         document.getElementById('menuContents').classList.toggle('nav--hidden')
     }
     else {
         //Switch to menu closed
         menuClosed = true;
-        e.currentTarget.src = "./assets/shared/mobile/menu.svg";
+        e.target.src = "./assets/shared/mobile/menu.svg";
         document.getElementById('menuContents').classList.toggle('nav--hidden')
     }
 });
