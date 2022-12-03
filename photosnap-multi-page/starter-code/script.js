11e5be36e6e2b0101eaf35513e1bc41cecfc94dd
Clay Stuart <clay.stuart@gmail.com>
2022-12-02 23:22:55 -0500

    Simplified header - removed <ul> technique.

--- photosnap-multi-page/starter-code/script.js
+++ photosnap-multi-page/starter-code/script.js
@@ -1,16 +1,16 @@
 // CSS Burger Menu animation
 let menuClosed = true;
-document.querySelector('#menuSwitch').addEventListener('click', (e) => {
+document.querySelector('#hamburgerIcon').addEventListener('click', (e) => {
     if (menuClosed == true) {
         //Switch to menu open
         menuClosed = false;
         e.target.src = "./assets/shared/mobile/close.svg";
-        document.getElementById('menuContents').classList.toggle('nav--hidden')
+        document.getElementById('menuContents').classList.toggle('menu--closed')
     }
     else {
         //Switch to menu closed
         menuClosed = true;
         e.target.src = "./assets/shared/mobile/menu.svg";
-        document.getElementById('menuContents').classList.toggle('nav--hidden')
+        document.getElementById('menuContents').classList.toggle('menu--closed')
     }
 });
