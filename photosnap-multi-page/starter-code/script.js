24ca1137646538aea0f047d4bab16a08bd1e91dc
Clay Stuart <clay.stuart@gmail.com>
2022-12-03 00:13:46 -0500

    Moved toggle javascript from pricing.html tp script.js

--- photosnap-multi-page/starter-code/script.js
+++ photosnap-multi-page/starter-code/script.js
@@ -12,3 +12,11 @@ hamburgerIcon.addEventListener('click', (e) => {
         menuContents.classList.toggle('menu--closed')
     }
 });
+
+term__toggleSwitch.addEventListener('change', () => {
+    month__Toggle.classList.toggle('toggle--selected');
+    year__Toggle.classList.toggle('toggle--selected');
+
+    for (let i of document.querySelectorAll('.price__section'))
+        i.classList.toggle('price__section--hidden');
+});
