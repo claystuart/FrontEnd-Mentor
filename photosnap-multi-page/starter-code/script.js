b7b50cb49b151916d5d2b03ffc5fc3b279e8492f
Clay Stuart <clay.stuart@gmail.com>
2022-12-02 23:26:53 -0500

    Simplified and streamlined Burger Menu javascript.

--- photosnap-multi-page/starter-code/script.js
+++ photosnap-multi-page/starter-code/script.js
@@ -1,16 +1,14 @@
 // CSS Burger Menu animation
-let menuClosed = true;
-document.querySelector('#hamburgerIcon').addEventListener('click', (e) => {
-    if (menuClosed == true) {
+
+hamburgerIcon.addEventListener('click', (e) => {
+    if (menuContents.classList.contains('menu--closed')) {
         //Switch to menu open
-        menuClosed = false;
         e.target.src = "./assets/shared/mobile/close.svg";
-        document.getElementById('menuContents').classList.toggle('menu--closed')
+        menuContents.classList.toggle('menu--closed')
     }
     else {
         //Switch to menu closed
-        menuClosed = true;
         e.target.src = "./assets/shared/mobile/menu.svg";
-        document.getElementById('menuContents').classList.toggle('menu--closed')
+        menuContents.classList.toggle('menu--closed')
     }
 });
