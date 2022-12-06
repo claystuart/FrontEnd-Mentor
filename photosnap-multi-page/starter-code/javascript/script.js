452a2234676fc09b823c0ce416f7bfbce0ee4b15
Clay Stuart <clay.stuart@gmail.com>
2022-12-06 15:55:58 -0500

    Moving slider control script from scripts.js to pricing.html

--- photosnap-multi-page/starter-code/javascript/script.js
+++ photosnap-multi-page/starter-code/javascript/script.js
@@ -5,7 +5,6 @@
  * 
  * Description: Toggles the hamburger menu open and shut
  */
-// hamburgerIcon.addEventListener('click', (e) => {
 hamburgerIcon.addEventListener('click', (e) => {
     if (menuContents.classList.contains('menu--closed')) {
         //Switch to menu open
@@ -17,19 +16,4 @@ hamburgerIcon.addEventListener('click', (e) => {
         e.target.src = "./assets/shared/mobile/menu.svg";
         menuContents.classList.toggle('menu--closed')
     }
-});
-
-/**
- * Location: pricing.html > div.toggle__switch__container
- * 
- * Description: Changes the highlighting for the 
- * monthly and yearly label and toggles the pricing visibility 
- * between monthly and yearly prices.
- */
-term__toggleSwitch.addEventListener('change', () => {
-    month__Toggle.classList.toggle('toggle--selected');
-    year__Toggle.classList.toggle('toggle--selected');
-
-    for (let i of document.querySelectorAll('.price__section'))
-        i.classList.toggle('price__section--hidden');
 });
