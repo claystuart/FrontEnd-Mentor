9ef702b7f0d7707ea878af1e1965c4f72555ea8f
Clay Stuart <clay.stuart@gmail.com>
2023-08-03 11:36:32 -0400

    Implement results section and finish mobile part.

--- rock-paper-scissors/rockPaperScissors.js
+++ rock-paper-scissors/rockPaperScissors.js
@@ -17,22 +17,47 @@ document.getElementById("scissors").addEventListener("click", (e) => {
 
 // Main function: actually play the game
 function playGame() {
-  console.log(`Player choose ${player}`);
-  computer = getComputerChoice();
-  console.log(`Computer choose ${computer}`);
+  // See who won
+  computer = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+  console.log(`Player -> ${player} and Computer -> ${computer}`);
   let winner = seeWhoWins();
   console.log(`Winner is ${winner}`);
 
+  // Update score display
   if (winner === "player") score++;
   if (winner === "computer") score--;
   document.getElementById("scores").innerHTML = score;
+
+  // Hide container and Display results-container
+  toggleDisplay();
+  DisplayResults();
+
+  // update Who Won Message
+  let whoWonText = document.getElementById("who_won_text");
+  if (winner == "player") whoWonText.innerHTML = "YOU WIN";
+  else if (winner == "tie") whoWonText.innerHTML = "TIE GAME";
+  else whoWonText.innerHTML = "YOU LOSE";
 }
 
-// helper function: get computer's choice
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  let choice = ["rock", "paper", "scissors"][randomNumber];
-  return choice;
+// helper function: update results area
+function DisplayResults() {
+  // Update player's icon
+  document.getElementById(
+    "human-result"
+  ).classList = `option human-result ${player}-result`;
+
+  document.getElementById(
+    "human-result-img"
+  ).src = `./images/icon-${player}.svg`;
+
+  // Update computer's icon
+  document.getElementById(
+    "computer-result"
+  ).classList = `option human-result ${computer}-result`;
+
+  document.getElementById(
+    "computer-result-img"
+  ).src = `./images/icon-${computer}.svg`;
 }
 
 // helper function: see who wins
@@ -47,6 +72,20 @@ function seeWhoWins() {
   else return "computer";
 }
 
+// helper function: toggle start-box and results-box
+function toggleDisplay() {
+  document.getElementById("start-box").classList.toggle("hide");
+  document.getElementById("results-container").classList.toggle("hide");
+  document.getElementById("play_again_box").classList.toggle("hide");
+  document.getElementById("main").style.backgroundImage = "none";
+}
+
+// Play Again Button
+document.getElementById("play_again_button").addEventListener("click", (e) => {
+  toggleDisplay();
+});
+
+/*****************************************************************************/
 // RULES functionality
 document.getElementById("rules_button").addEventListener("click", (e) => {
   toggleContainers();
@@ -56,6 +95,7 @@ document.getElementById("rules_exit_button").addEventListener("click", (e) => {
   toggleContainers();
 });
 
+// Hide and Reveal main container and rules-container
 function toggleContainers() {
   let rules = document.getElementById("rules-container");
   let container = document.getElementById("container");
